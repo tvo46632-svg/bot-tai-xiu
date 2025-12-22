b997edc6260157c0fd4886dd1afcaefe769cacac
tvo46632-svg <tvo46632@gmail.com>
2025-12-22 17:10:44 +0700

    Update users.js

--- users.js
+++ users.js
@@ -1,15 +1,1 @@
-{
-  "name": "discord-bot",
-  "version": "1.0.0",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "discord.js": "^14.0.0",
-    "lowdb": "^5.0.0"
-  },
-  "engines": {
-    "node": "22.x"
-  }
-}
+{}
