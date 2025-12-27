68694f0f33818c20f7cae2b31f204f7ad5485741
tvo46632-svg <tvo46632@gmail.com>
2025-12-27 22:06:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -539,7 +539,7 @@ async function cmdTungxu(message, args) {
 //      TAI XIU MULTIPLAYER
 // =====================
 async function cmdTaixiu(message) {
-    const gifWaiting = "https://media1.tenor.com/m/5PepR8rD4U0AAAAC/throwing-dice-quavo.gif";
+    const gifWaiting = "https://media.tenor.com/5PepR8rD4U0AAAAC/throwing-dice-quavo.gif";
     const gifRolling = "https://media.tenor.com/acXyDcloqNoAAAAi/dice-1-dice.gif";
 
     // 1. Khởi tạo danh sách người chơi trong phiên này
